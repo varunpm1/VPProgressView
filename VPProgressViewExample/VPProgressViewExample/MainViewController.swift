299b792daa9ad25d5fddb116318e0433839848b4
Varun P M <varun.pm@ymedialabs.com>
2016-08-01 13:38:39 +0530

    Initial Commit

---
+++ VPProgressViewExample/VPProgressViewExample/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  VPProgressViewExample
+//
+//  Created by Varun on 01/08/16.
+//  Copyright © 2016 VPM. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
